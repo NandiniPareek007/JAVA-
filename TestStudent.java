c8dd3fbfb99e7268cbcd751ac9fe32d845216a63
NandiniPareek007 <nandinipareek2711@gmail.com>
2025-04-02 00:00:31 +0530

    Delete TestStudent.java

--- TestStudent.java
+++
@@ -1,9 +0,0 @@
- class TestStudent {
-    public static void main (String []args )
-    {
-        StudentUsingScanner st=new StudentUsingScanner();
-        st.getdetails();
-        st.putdetails();
-    }
-}
-// This main class is linked with the class StudentUsingScanner.java
