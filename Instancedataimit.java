5077e2f5f55b20588c974ef74c48c9cf787104bb
NandiniPareek007 <nandinipareek2711@gmail.com>
2025-04-01 21:27:24 +0530

    Add files via upload

---
+++ Instancedataimit.java
@@ -0,0 +1,19 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+class Instancedataimit {
+   int num1 = 15;
+   int num2 = 3;
+
+  
+
+   void multi() 
+   {
+      int mul = num1 * num2;
+      System.out.println("the multiplication is =" + mul);
+   }
+
+   public static void main(String[] args ) 
+   {
+      Instancedataimit Id = new Instancedataimit();
+      Id.multi();
+   }
+}
